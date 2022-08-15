f4e70329edf4c5e5b210e4e505fde169e9b23445
blackingg <93845490+blackingg@users.noreply.github.com>
2022-08-15 12:23:36 +0100

    b

---
+++ orders.js
@@ -0,0 +1,38 @@
+[
+    {
+        ProductName: 'Foldable Mini Drone',
+        ProductNumber: '85631',
+        Payment: 'Due',
+        Status: 'Pending',
+    },
+    {
+        ProductName: 'Lavender KF182 Drone',
+        ProductNumber: '33489',
+        Payment: 'Due',
+        Status: 'Pending',
+    },
+    {
+        ProductName: 'GPS 4K Drone',
+        ProductNumber: '85631',
+        Payment: 'Due',
+        Status: 'Pending',
+    },
+    {
+        ProductName: 'Cinematic Drone',
+        ProductNumber: '88654',
+        Payment: 'Due',
+        Status: 'Pending',
+    },
+    {
+        ProductName: 'Ruko F11 Pro Drone',
+        ProductNumber: '85631',
+        Payment: 'Refunded',
+        Status: 'Declined',
+    },
+    {
+        ProductName: 'Military Killer Drone',
+        ProductNumber: '96755',
+        Payment: 'Paid',
+        Status: 'Delivered',
+    },
+]
